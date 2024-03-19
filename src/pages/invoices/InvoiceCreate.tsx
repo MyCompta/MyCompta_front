ed11928b677364b2beb1811765551d580e060e3f
LoulergueC <clement@loulergue.fr>
2024-03-19 14:04:13 +0000

    fix auto-reference to invoice

--- src/pages/invoices/InvoiceCreate.tsx
+++ src/pages/invoices/InvoiceCreate.tsx
@@ -1,6 +1,7 @@
 import "./Invoice.css";
+import Invoice from "../../components/invoice/Invoice";
 
-export default function Invoice() {
+export default function InvoiceCreate() {
   return (
     <div>
       <h1>Create Invoice</h1>
