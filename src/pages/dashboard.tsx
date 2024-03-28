1f67307811b01ad9abd665ccb28a1d9ab7a5aee3
mathieu.amacher <amacher_mathieu@hotmail.com>
2024-03-28 15:54:43 +0100

    first graph

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,30 @@
+import { BarChart } from '../components/charts/Barcharts'
+import { Doughnuts } from '../components/charts/DoughnutsCharts'
+import { LineCharts } from '../components/charts/LineCharts'
+import { PieCharts } from '../components/charts/PieCharts'
+
+import "./index.scss"
+
 const Dashboard = () => {
 
   return (
-    <h1> Ceci est le Dashboard quand on est logged</h1>
+    <div className="dashboard_page">
+      <h1>Dashboard</h1>
+      <div className="chartcontainer">
+        <div className="barchart_on_dashboard">
+          <BarChart />
+        </div>
+        <div className="doughnuts_on_dashboard">
+          <Doughnuts />
+        </div>
+        <div className="piecharts_on_dashboard">
+          <PieCharts />
+        </div>
+        <div className="linecharts_on_dashboard">
+          <LineCharts />
+        </div>
+      </div>
+    </div>
   )
 }
 
